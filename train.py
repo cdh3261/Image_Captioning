43585f785ed0e8431632451b01882ba0488b2dd5
cdh3261 <cdh3261@naver.com>
2020-06-02 20:43:51 +0900

    update

--- train.py
+++ train.py
@@ -166,57 +166,6 @@ loss_plot = training(start_epoch, dataset, num_steps)
 preprocess.pltshow(loss_plot)
 
 
-
-# def evaluate(image):
-#     attention_plot = np.zeros((max_length, attention_features_shape))
-
-#     hidden = decoder.reset_state(batch_size=1)
-
-#     temp_input = tf.expand_dims(preprocess.load_image(image)[0], 0)
-#     img_tensor_val = image_features_extract_model(temp_input)
-#     img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
-
-#     features = encoder(img_tensor_val)
-
-#     dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
-#     result = []
-
-#     for i in range(max_length):
-#         predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
-
-#         attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
-
-#         predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
-#         result.append(tokenizer.index_word[predicted_id])
-
-#         if tokenizer.index_word[predicted_id] == '<end>':
-#             return result, attention_plot
-
-#         dec_input = tf.expand_dims([predicted_id], 0)
-
-#     attention_plot = attention_plot[:len(result), :]
-#     return result, attention_plot
-
-
-# def plot_attention(image, result, attention_plot, real_caption, pre_caption):
-#     temp_image = np.array(Image.open(image))
-
-#     fig = plt.figure(figsize=(10, 8))
-
-
-#     len_result = len(result)
-#     for l in range(len_result):
-#         temp_att = np.resize(attention_plot[l], (8, 8))
-#         ax = fig.add_subplot(len_result//2, len_result//2, l+1)
-#         ax.set_title(result[l])
-#         img = ax.imshow(temp_image)
-#         ax.imshow(temp_att, cmap='gray', alpha=0.6, extent=img.get_extent())
-
-#     plt.tight_layout()
-#     # plt.xlabel('real_caption: ' + real_caption + '\n' + 'predict_caption: ' + pre_caption)
-#     plt.show()
-
-
 # captions on the validation set
 # 랜덤한 이미지 하나 선택
 rid = np.random.randint(0, len(img_name_val))
